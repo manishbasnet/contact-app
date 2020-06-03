021bac7e00d65f93df617b88fb3049a4aa439fb9
Manish Basnet <manish.basnetnp@gmail.com>
2020-06-03 21:06:53 +0545

    removed apikeys

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -3,12 +3,12 @@
 
 
   export default {
-    apiKey: "AIzaSyDYw5D4jFMhj6DcZbsLIve7Kbs8D2yzJHg",
-    authDomain: "cwrn-db-8a8c2.firebaseapp.com",
-    databaseURL: "https://cwrn-db-8a8c2.firebaseio.com",
-    projectId: "cwrn-db-8a8c2",
-    storageBucket: "cwrn-db-8a8c2.appspot.com",
-    messagingSenderId: "210495459673",
-    appId: "1:210495459673:web:d6bca0181bf82da0617c48"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   
